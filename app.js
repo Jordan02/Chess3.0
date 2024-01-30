9abeb221aa45c1e85342338d586ef830f34754db
Jordan Mills <Mills-J9@ulster.ac.uk>
2024-01-30 03:02:34 +0000

    board function added

--- app.js
+++ app.js
@@ -2,3 +2,43 @@ const gameBoard = document.querySelector("#gameboard")
 const playerDisplay = document.querySelector("#player")
 const infoDisplay = document.querySelector("#info-display")
 const width = 8
+
+
+const startPieces = [
+
+  bR, bN, bB, bQ, bK, bB, bN, bR,
+  bP, bP, bP, bP, bP, bP, bP, bP, 
+  '', '', '', '', '', '', '', '',
+  '', '', '', '', '', '', '', '',
+  '', '', '', '', '', '', '', '',
+  '', '', '', '', '', '', '', '',
+  wP, wP, wP, wP, wP, wP, wP, wP, 
+  wR, wN, wB, wQ, wK, wB, wN, wR
+]
+
+// note const variables may need to change if ever we want to change peice svg to another style
+
+
+function createBoard() {
+
+  startPieces.forEach((startpiece,i)=>{
+
+    const square = document.createElement("div")
+    square.classList.add("square")
+    square.setAttribute("square-id", i)
+    square.append(returnImage(startpiece))
+
+    const row = Math.floor((63 - i)/ 8) +1
+
+    if( row % 2 === 0) {
+      square.classList.add(i % 2  === 0 ? "white" : "black")
+    } else {
+      square.classList.add(i % 2  === 0 ? "black" : "white")
+    }
+
+    gameBoard.append(square)
+
+  })
+}
+
+createBoard()
--- peices.js
+++ peices.js
@@ -1,14 +1,41 @@
-const king = getPeiceImage()
+const bR = 'src/peices/merida/bR.svg'
+const bN = 'src/peices/merida/bN.svg'
+const bB = 'src/peices/merida/bB.svg'
+const bQ = 'src/peices/merida/bQ.svg'
+const bK = 'src/peices/merida/bK.svg'
+const bP = 'src/peices/merida/bP.svg'
+
+const wR = 'src/peices/merida/wR.svg'
+const wN = 'src/peices/merida/wN.svg'
+const wB = 'src/peices/merida/wB.svg'
+const wQ = 'src/peices/merida/wQ.svg'
+const wK = 'src/peices/merida/wK.svg'
+const wP = 'src/peices/merida/wP.svg'
 
 
+function returnImage(url) {
 
-function getPeiceImage () {
+  if (url === '') {
+    return '';
+  } else {
 
-  fetch('src\\peices\\cburnett\\bP.svg')
+    const image = document.createElement("div")
+    image.classList.add("piece")
+  
+    const svgfile = fetch(url)
     .then(response => response.text())
-    .then(svgString => {
-        // Use svgString as needed in your JavaScript code
-        console.log(svgString);
-    })
-    .catch(error => console.error('Error fetching peice SVG:', error));
+    .then(text => {image.innerHTML = text})
+  
+    return image
+  }
 }
+
+
+
+
+
+
+
+
+
+
