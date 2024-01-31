63bf50a2d025d1b00ec3b7dd9cf7abff786fedf1
Jordan Mills <Mills-J9@ulster.ac.uk>
2024-01-31 22:44:08 +0000

    u

--- app.js
+++ app.js
@@ -4,16 +4,18 @@ const infoDisplay = document.querySelector("#info-display")
 const width = 8
 
 
-const startPieces = [
 
-  bR, bN, bB, bQ, bK, bB, bN, bR,
-  bP, bP, bP, bP, bP, bP, bP, bP, 
+const startPieces2 = [
+
+  'bR', 'bN', 'bB', 'bQ', 'bK', 'bB', 'bN', 'bR',
+  'bP', 'bP', 'bP', 'bP', 'bP', 'bP', 'bP', 'bP',
   '', '', '', '', '', '', '', '',
   '', '', '', '', '', '', '', '',
   '', '', '', '', '', '', '', '',
   '', '', '', '', '', '', '', '',
-  wP, wP, wP, wP, wP, wP, wP, wP, 
-  wR, wN, wB, wQ, wK, wB, wN, wR
+  'wP', 'wP', 'wP', 'wP', 'wP', 'wP', 'wP', 'wP',
+  'wR', 'wN', 'wB', 'wQ', 'wK', 'wB', 'wN', 'wR'
+  
 ]
 
 // note const variables may need to change if ever we want to change peice svg to another style
@@ -21,13 +23,19 @@ const startPieces = [
 
 function createBoard() {
 
-  startPieces.forEach((startpiece,i)=>{
-
+  startPieces2.forEach((startpiece,i)=>{
+    
+    //creating  square and adding peices
     const square = document.createElement("div")
     square.classList.add("square")
     square.setAttribute("square-id", i)
-    square.append(returnImage(startpiece))
 
+    square.append(createPlayer(startpiece))
+
+    startPieces2[i] !== ''? square.querySelector('.player').setAttribute("id", i) : {}
+
+
+    // coloring square by class
     const row = Math.floor((63 - i)/ 8) +1
 
     if( row % 2 === 0) {
@@ -36,6 +44,8 @@ function createBoard() {
       square.classList.add(i % 2  === 0 ? "black" : "white")
     }
 
+    
+    
     gameBoard.append(square)
 
   })
--- peices.js
+++ peices.js
@@ -13,30 +13,49 @@ const wK = 'src/peices/merida/wK.svg'
 const wP = 'src/peices/merida/wP.svg'
 
 
-function returnImage(url) {
 
-  if (url === '') {
+function createPlayer(pieceID) {
+
+  if (pieceID === '') {
     return '';
   } else {
 
     // create player object
-    const image = document.createElement("div")
-    image.classList.add("player")
-    image.setAttribute('kills', 0)
-  
-    // const svgfile = fetch(url)
+    const player = document.createElement("div")
+    player.classList.add("player")
+    player.setAttribute("type", pieceID)
+    player.setAttribute('kills', 0)
+
+    // const svgfile = fetch(pieceID)j
     // .then(response => response.text())
     // .then(text => {image.innerHTML = text}) 
 
+    // create selectable object
+    const selection = document.createElement("div")
+    selection.classList.add("draggable")
+    selection.setAttribute("draggable", true)
+    player.append(selection)
+
+
+    // make selection object draggable
+    selection.setAttribute("draggable",true)
+    selection.addEventListener("dragstart",dragStart)
+    selection.addEventListener("dragover",dragOver)
+    selection.addEventListener("drag", drag)
+    selection.addEventListener("drop",dragDrop)
+
+
     //add peice image to player object with it's own class
     const piece = document.createElement("img")
+    const url = 'src/peices/merida/' + pieceID + '.svg'
     piece.setAttribute("src", url)
     piece.classList.add("piece")
-    image.append(piece)
+    player.append(piece)
 
     //create perks container and add perks
     const perks = document.createElement("div")
     perks.classList.add("perks")
+    // perks.setAttribute("draggable",false)
 
     const perk1 = document.createElement("img")
     perk1.setAttribute("src","src/killstreaks/stockholm_syndrome.png")
@@ -50,18 +69,54 @@ function returnImage(url) {
     perk3.setAttribute("src","src/killstreaks/msc_trinity.png")
     perks.append(perk3)
 
-    image.append(perks)
+    player.append(perks)
 
-    return image
+    return player
   }
 }
 
+let startPositionID
+let draggedElement
+let endPositionID
 
 
+/**
+ * @param {DragEvent} e - The drag event object
+ */
 
+function dragStart(e){
 
+  // selection object should be on front, the parent parent (grandfather node) is the square id which is stored
+  startPositionID = startPositionID = e.target.parentNode.parentNode.getAttribute('square-id')
+  console.log(startPositionID)
+  draggedElement = e.target
+}
 
+function drag(e){
 
+  const elementName = e.target.parentNode.getAttribute("id")
+  const a = document.getElementById(elementName)
+  
+  a.style.left = 0
+  a.style.top = 0
 
+}
 
+function dragOver(e){
 
+  // this prevents default option that will contiously log object being dragged over
+  e.preventDefault()
+  // from selection, we get a reference to player id, and move it cursor location
+  const elementName = e.target.parentNode.getAttribute("id")
+ 
+  console.log(elementName)
+  // e.target.parentNode.
+}
+
+function dragDrop(e){
+  // this stops drop from occuring in two squares/ imporant
+  //e.stopPropgation() 
+  console.log(e.target.parentNode.parentNode.getAttribute('square-id'))
+  //e.target.append(draggedElement)
+ 
+}
