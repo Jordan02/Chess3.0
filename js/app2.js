6d450098c8557fa99f30342a03564c6c5096d28a
Jordan Mills <Mills-J9@ulster.ac.uk>
2024-02-11 01:44:57 +0000

    messing about wit it ah ahahaa

--- js/app2.js
+++ js/app2.js
@@ -6,8 +6,14 @@ const container = document.querySelector("#container2")
 function main() {
 
   var board = new chessboard('GB2','#container2')
-
   var board2 = new chessboard('GB3','#container2')
+  var board2 = new chessboard('GB4','#container2')
+  var board2 = new chessboard('GB5','#container2')
+  var board = new chessboard('GB2','#container2')
+  var board2 = new chessboard('GB3','#container2')
+  var board2 = new chessboard('GB4','#container2')
+  var board2 = new chessboard('GB4','#container2')
+  var board2 = new chessboard('GB4','#container2')
   
 }
 
